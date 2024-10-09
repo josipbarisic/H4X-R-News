c2b4a1d8f7a3043d0c883d631b0fe4eda2d4e66b
josipbarisic <josip.barisic1998@gmail.com>
2024-10-09 12:29:05 +0200

    Initial Commit

---
+++ H4XØR News/H4X_R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X_R_NewsApp.swift
+//  H4XØR News
+//
+//  Created by Josip Barisic on 09.10.2024..
+//
+
+import SwiftUI
+
+@main
+struct H4X_R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ H4XØR NewsTests/H4X_R_NewsTests.swift
@@ -0,0 +1,17 @@
+//
+//  H4X_R_NewsTests.swift
+//  H4XØR NewsTests
+//
+//  Created by Josip Barisic on 09.10.2024..
+//
+
+import Testing
+@testable import H4X_R_News
+
+struct H4X_R_NewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
